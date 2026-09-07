7711515043b82b6f671e527dec06284959881d6d
teguhhariyono0987655gmail.com <nabilkeceebet@gmail.com>
2026-09-07 22:33:39 +0700

    updte

--- admin/login_admin.js
+++ admin/login_admin.js
@@ -5,7 +5,7 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     const passwordInput = document.getElementById('password').value;
 
     try {
-        const response = await fetch('http://localhost:3000/api/login', {
+        const response = await fetch('/api/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
